801d4e669823967995a6365252759d02e3e511bc
Jmmochkof <jose.mochkofsky@mi.unc.edu.ar>
2024-09-21 16:40:22 -0300

    reestructurado de carpeta, crear partidas, y tests

--- src/entities/game/game_utils.py
+++ src/entities/game/game_utils.py
@@ -1,11 +1,11 @@
-from entities.game.game import Game
+from ..game.game import Game
 
 games = {}
-
 #adds a game to the games dictionary
 #TODO: database functionality
 def add_game(new_game):
-    games[new_game.unique_id] = new_game
+    if(not new_game.unique_id in games):
+        games[new_game.unique_id] = new_game
 
 def print_game(game):
     print(game.name + '\n' + game.unique_id)
---
+++ tests/test_add_and_get_game.py
@@ -0,0 +1,21 @@
+import unittest
+from src.entities.game.game import Game
+from src.entities.game.game_utils import add_game,get_games
+from src.entities.player.player import Player
+
+
+class test_game_utils(unittest.TestCase):
+    def test_add_and_get_game(self):
+        darth_vader = Player(name = "luke")
+        added = []
+        for _ in range(100):
+            new_game = Game(name = "DeathStar", creator = darth_vader)
+            added.append(new_game.unique_id)
+            add_game(new_game)
+        all_games = get_games()
+        for game in added:
+            assert game in all_games
+    
+
+if __name__ == "__main__":
+    unittest.main()
