1e9727a15c82034155c49f6e648917cae7978998
Jmmochkof <jose.mochkofsky@mi.unc.edu.ar>
2024-09-22 17:18:10 -0300

    listar partidas endpoint y test

--- src/schemas/game_schemas.py
+++ src/schemas/game_schemas.py
@@ -1,6 +1,7 @@
 # back/schemas/game_schemas.py
 
 from pydantic import BaseModel
+from typing import List
 
 # Formato de POST a /games
 class CreateGameRequest(BaseModel):
@@ -11,3 +12,7 @@ class CreateGameRequest(BaseModel):
 class CreateGameResponse(BaseModel):
     player_id: str
     game_id: str
+
+class GameInResponse(BaseModel):
+    game_id: str
+    game_name: str
