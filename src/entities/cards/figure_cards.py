b4515f148a7582135823c192a5679e0030436526
Jmmochkof <jose.mochkofsky@mi.unc.edu.ar>
2024-09-29 08:52:37 -0300

    starting with cards

--- src/entities/cards/figure_cards.py
+++ src/entities/cards/figure_cards.py
@@ -1,7 +1,6 @@
 #all squares needed to form a figure
 #to check if a set of squares matches, sort and match the first remembering the diference in x and y. Check if all other squares given -<dx,dy> match as well
-#TODO: complete for all cardtypes
-figure_card_hard_types = [[[0,-1],[0,0],[-1,-1],[1,-1],[0,1]],
+figure_card_types = [[[0,-1],[0,0],[-1,-1],[1,-1],[0,1]],
                      [[0,0],[0,1],[1,1],[1,2],[1,3]],
                      [[0,0],[0,-1],[1,-1],[1,-2],[1,-3]],
                      [[0,0],[1,0],[1,1],[2,1],[2,2]],
--- src/entities/db/models.py
+++ src/entities/db/models.py
@@ -18,8 +18,11 @@ class Player(Base):
     unique_id = Column(String, primary_key=True)
     name = Column(String, nullable=False)
     games = relationship('Game', secondary=game_player_association, back_populates='players')
+<<<<<<< HEAD
     
     # Access all cards that a player has
+=======
+>>>>>>> af7c8aa (starting with cards)
     figure_cards = relationship('Figure_card', back_populates='player', cascade="all, delete-orphan")
     movement_cards = relationship('Movement_card', back_populates='player', cascade="all, delete-orphan")
 
@@ -34,6 +37,7 @@ class Game(Base):
 
 class Figure_card(Base):
     __tablename__ = 'Figure_cards'
+<<<<<<< HEAD
     unique_id = Column(String, primary_key=True)  # Ensure a primary key exists
     card_type = Column(Integer)
     state = Column(String, nullable=False)  # 'Drawn', 'Not drawn', 'Blocked'
@@ -54,6 +58,19 @@ class Movement_card(Base):
     game = relationship('Game')
 
 #NEED A FUNCTION TO CREATE CARDS
+=======
+    card_type = Column(Integer)
+    # 'Drawn' 'Not drawn' 'Blocked'
+    state = Column(String, nullable=False)
+    player = relationship('Player', back_populates='figure_cards')
+    pass
+
+class Movement_card(Base):
+    __tablename__ = 'Movement_cards'
+    card_type = Column(Integer)
+    player = relationship('Player', back_populates='Movement_cards')
+    pass
+>>>>>>> af7c8aa (starting with cards)
 
 # Crea las tablas en la base de datos
 Base.metadata.create_all(engine)
