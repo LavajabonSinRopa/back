1360016f724ea71f43c0229260ef916178699963
ZorxExt <francisco.sabate@mi.unc.edu.ar>
2024-09-30 14:16:06 -0300

    Update test_game_methods.py

--- tests/test_game_methods.py
+++ tests/test_game_methods.py
@@ -3,12 +3,12 @@ from src.entities.game.game_utils import add_game,get_games,delete_all,start_gam
 from src.entities.player.player_utils import add_player
 
 class test_game_utils(unittest.TestCase):
-    # def test_add_and_get_game(self):
-    #     delete_all()
-    #     darth_id = add_player(player_name="darthVader")
-    #     for added in range(100):
-    #         assert added == len(get_games())
-    #         add_game(game_name = "DeathStar", creator_id = darth_id)
+    def test_add_and_get_game(self):
+        delete_all()
+        darth_id = add_player(player_name="darthVader")
+        for added in range(100):
+            assert added == len(get_games())
+            add_game(game_name = "DeathStar", creator_id = darth_id)
     
     def test_start_game(self):
         delete_all()
