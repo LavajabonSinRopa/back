882427fb74113e17adc6fc24fdd61fd7ea0de91c
Jmmochkof <jose.mochkofsky@mi.unc.edu.ar>
2024-10-07 15:32:15 -0300

    bugfix

--- src/interfaces/game_endpoints.py
+++ src/interfaces/game_endpoints.py
@@ -51,12 +51,9 @@ async def join_game(game_id: str, request: JoinGameRequest):
     player_id = add_player(player_name=request.player_name)
     add_to_game(player_id=player_id, game_id=game_id)
     game_socket_manager.join_player_to_game_map(game_id,player_id)
-
-    # Get name of the player
-    player_name = game['player_names'][game['players'].index(request.player_id)]
     
     # Avisar a los sockets de la partida sobre el jugador que abandona.
-    await game_socket_manager.broadcast_game(game_id,{"type":"PlayerJoined","payload": {'player_id' : request.player_id, 'player_name': player_name}})
+    await game_socket_manager.broadcast_game(game_id,{"type":"PlayerJoined","payload": {'player_id' : player_id, 'player_name': request.player_name}})
 
     # Devolver ID unico de jugador para la partida
     return JoinGameResponse(player_id=player_id)
